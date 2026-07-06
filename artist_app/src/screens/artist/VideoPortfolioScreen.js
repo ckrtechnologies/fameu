893fc8dbdf07a3170967f3b1199fac26c4313d01
mallikera-spec <chandanmallik@gmail.com>
2026-07-06 21:14:41 +0530

    feat: add KeyboardAvoidingView to VideoPortfolioScreen and include project tracking documentation

--- artist_app/src/screens/artist/VideoPortfolioScreen.js
+++ artist_app/src/screens/artist/VideoPortfolioScreen.js
@@ -1,6 +1,6 @@
 import { GlobalAlert } from '../../components/core/GlobalAlert';
 import React, { useState } from 'react';
-import { View, StyleSheet, FlatList, TouchableOpacity, Dimensions, Platform, Image, TextInput, ActivityIndicator, Alert } from 'react-native';
+import { View, StyleSheet, FlatList, TouchableOpacity, Dimensions, Platform, Image, TextInput, ActivityIndicator, Alert, KeyboardAvoidingView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ArrowLeft, Play, X, Volume2, VolumeX, Trash2 } from 'lucide-react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
